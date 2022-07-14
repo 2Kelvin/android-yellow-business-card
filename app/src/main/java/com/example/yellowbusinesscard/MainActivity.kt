daf6b776de9b2dfbf3d366a5a9e47141f90b9aa2
2Kelvin <kvin.mwangi254@gmail.com>
2022-07-14 22:56:53 +0300

    business card complete

--- app/src/main/java/com/example/yellowbusinesscard/MainActivity.kt
+++ app/src/main/java/com/example/yellowbusinesscard/MainActivity.kt
@@ -5,19 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.Phone
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,65 +41,105 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+val fontNorm = FontFamily(
+    Font(R.font.spacegrotesk_medium)
+)
+
+val fontMono = FontFamily(
+    Font(R.font.spacemono_regular)
+)
+
 @Composable
 fun BusinessCard() {
     Row(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Cyan),
+            .background(Color.Black),
         verticalAlignment = Alignment.CenterVertically
     ) {
         // section 1
-        Column(Modifier.weight(1f)) {
+        Column(
+            Modifier.weight(1f),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Text(
-                text = "Kelvin",
+                text = stringResource(R.string.name),
                 fontWeight = FontWeight.Bold,
-                fontSize = 43.sp
+                fontSize = 37.sp,
+                color = colorResource(R.color.bright_yellow),
+                fontFamily = fontNorm
             )
             Text(
-                text = "Android Developer",
-                fontSize = 18.sp
+                text = stringResource(R.string.dev_status),
+                fontSize = 16.sp,
+                color = colorResource(R.color.norm_yellow),
+                fontFamily = fontMono
             )
         }
+
+        Spacer(modifier = Modifier.width(5.dp))
+
+        // divider
+        Divider(
+            color = colorResource(R.color.norm_yellow),
+            modifier = Modifier
+                .height(110.dp)
+                .width(2.dp)
+        )
+
         // section 2
-        Column(Modifier.weight(1f)) {
+        Column(
+            Modifier.weight(1f),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Row {
                 Icon(
                     imageVector = Icons.Outlined.Phone,
-                    contentDescription = "call",
-                    Modifier.weight(1f)
+                    contentDescription = null,
+                    Modifier.weight(1f),
+                    colorResource(R.color.norm_yellow)
                 )
                 Text(
-                    text = "123 456 789",
-                    Modifier.weight(3f)
+                    text = stringResource(R.string.phone_no),
+                    Modifier.weight(3f),
+                    color = colorResource(R.color.norm_yellow),
+                    fontFamily = fontNorm
                 )
             }
 
-            Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.height(20.dp))
 
             Row {
                 Icon(
                     imageVector = Icons.Outlined.Email,
-                    contentDescription = "email",
-                    Modifier.weight(1f)
+                    contentDescription = null,
+                    Modifier.weight(1f),
+                    colorResource(R.color.norm_yellow)
                 )
                 Text(
-                    text = "developer@gmail.com",
-                    Modifier.weight(3f)
+                    text = stringResource(R.string.email),
+                    Modifier.weight(3f),
+                    color = colorResource(R.color.norm_yellow),
+                    fontFamily = fontNorm
                 )
             }
 
-            Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.height(20.dp))
 
             Row {
                 Icon(
                     imageVector = Icons.Outlined.Home,
-                    contentDescription = "website",
-                    Modifier.weight(1f)
+                    contentDescription = null,
+                    Modifier.weight(1f),
+                    colorResource(R.color.norm_yellow)
                 )
                 Text(
-                    text = "myWebsite.com",
-                    Modifier.weight(3f)
+                    text = stringResource(R.string.website),
+                    Modifier.weight(3f),
+                    color = colorResource(R.color.norm_yellow),
+                    fontFamily = fontNorm
                 )
             }
         }
